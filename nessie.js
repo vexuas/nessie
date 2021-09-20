6e624f0fcf3c6bc3964ca50f8f5fcc882beba8b0
Gabriel R <42207245+vexuas@users.noreply.github.com>
2021-09-21 03:20:50 +0800

    Add messageCreate event handler (#4)
    * Add messageCreate event handler
    
    * Show current prefix when mentioning nessie
    
    * Add comments

--- nessie.js
+++ nessie.js
@@ -1,15 +1,46 @@
+/**
+ * https://discord.com/developers/docs/topics/gateway#list-of-intents For list of intents
+ * Newer versions of discordjs requires to list down intents to properly work
+ * Basically need to explicitly tell discord which events our bot needs
+ **/
 const Discord = require('discord.js');
-const nessie = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS]});
-//https://discord.com/developers/docs/topics/gateway#list-of-intents For list of intents
+const nessie = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES]});
 
+//Get config data from config folder
 const { defaultPrefix, token } = require('./config/nessie.json');
 
-nessie.login(token);
+nessie.login(token); //Login to discord with bot's token
 
+/**
+ * Event handler that fires once when nessie boots up and succesfully logs in
+ */
 nessie.once('ready', async () => {
   try {
     const testChannel = nessie.channels.cache.get('889212328539725824');
     testChannel && testChannel.send("I'm booting up! (◕ᴗ◕✿)");
+  } catch(e){
+    console.log(e);
+  }
+})
+
+nessie.on('messageCreate', async (message) => {
+  if (message.author.bot) return; //Ignore messages made by nessie
+  const nessiePrefix = defaultPrefix;
+
+  try {
+    /**
+     * Nessie checks if messages contains any mentions
+     * If it does and if one of the mentions contains nessie's user, returns a message with the current prefix i.e @Nessie
+     */
+    message.mentions.users.forEach((user) => {
+      if(user === nessie.user){
+        return message.channel.send('My current prefix is ' + '`' + `${nessiePrefix}` + '`');
+      }
+    });
+    //Ignores messages without a prefix
+    if(message.content.startsWith(defaultPrefix)){
+      await message.channel.send('hello!');
+    }
   } catch(e){
     console.log(e);
   }
