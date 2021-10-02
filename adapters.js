e21decb3627bbe6f7739489e5a8afd2dce91a5b3
Gabe R <rodriguezgab1601@gmail.com>
2021-10-03 04:56:55 +0800

    Create adapters for each map rotation

--- adapters.js
+++ adapters.js
@@ -1,14 +1,29 @@
 const axios = require('axios');
 const { apiKey } = require('./config/nessie.json');
 
+const url = `https://api.mozambiquehe.re/maprotation?version=2&auth=${apiKey}`
 
-exports.getCurrentMapRotations = async () => {
-  try {
-    const data = await axios.get(`https://api.mozambiquehe.re/maprotation?version=2&auth=${apiKey}`).then(response => {
-      return response.data;
-    })
-    return data;
-  } catch(e){
-    console.log(e); //Maybe add special error handling
-  }
+exports.getBattleRoyalePubs = async () => {
+  const data = await axios.get(url).then(response => {
+    return response.data.battle_royale;
+  })
+  return data;
+}
+exports.getBattleRoyaleRanked = async () => {
+  const data = await axios.get(url).then(response => {
+    return response.data.ranked;
+  })
+  return data;
+}
+exports.getArenasPubs = async () => {
+  const data = await axios.get(url).then(response => {
+    return response.data.arenas;
+  })
+  return data;
+}
+exports.getArenasRanked = async () => {
+  const data = await axios.get(url).then(response => {
+    return response.data.arenasRanked;
+  })
+  return data;
 }
