58dac3f0e74f6c78ace9eedfe9eda05354b23fc2
Gabe R <rodriguezgab1601@gmail.com>
2021-10-03 05:31:04 +0800

    Add comments

--- commands/maps/arenas.js
+++ commands/maps/arenas.js
@@ -1,11 +1,26 @@
+const { getArenasPubs, getArenasRanked } = require('../../adapters');
+
 module.exports = {
   name: 'arenas',
   description: 'Shows currrent map rotation for arenas mode',
   hasArguments: true,
   async execute({message, arguments}){
-    if(arguments === 'ranked'){
-      return await message.channel.send('Arenas Ranked map command');
+    message.channel.sendTyping();
+    try {
+      if(!arguments){
+        const data = await getArenasPubs();
+        const dataToSend = `Current Map: ${data.current.map}\nNext Map: ${data.next.map}`;
+        return message.channel.send(dataToSend);
+      } else{
+        if(arguments === 'ranked'){
+          const data = await getArenasRanked();
+          const dataToSend = `Current Map: ${data.current.map}\nNext Map: ${data.next.map}`;
+          return message.channel.send(dataToSend);
+        }
+        return message.channel.send("I don't understand that argument （・□・；）");
+      }
+    } catch(e){
+      console.log(e); //Add proper error handling someday
     }
-    return await message.channel.send('Arenas map command!');
   }
 }
