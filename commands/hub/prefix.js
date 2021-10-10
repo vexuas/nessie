ed805d5974e425cbc60fe2222bd976338a7d677d
Gabe R <rodriguezgab1601@gmail.com>
2021-10-10 17:35:06 +0800

    Create prefix command

---
+++ commands/hub/prefix.js
@@ -0,0 +1,32 @@
+//Reminder to add custom prefix before v3
+const { defaultPrefix } = require('../../config/nessie.json');
+
+const generateEmbed = (prefix) => {
+  const embed = {
+    color: 16776960,
+    fields: [
+      {
+        name: 'Current Prefix',
+        value: prefix,
+        inline: true
+      },
+      {
+        name: 'Usage Example',
+        value: `${prefix}help`,
+        inline: true
+      }
+    ]
+  };
+  return [embed];
+};
+
+module.exports = {
+  name: 'prefix',
+  description: 'Shows current prefix',
+  hasArguments: false,
+  execute({message}) {
+    const currentPrefix = defaultPrefix;
+    const embed = generateEmbed(currentPrefix);
+    message.channel.send({ embeds: embed });
+  }
+};
