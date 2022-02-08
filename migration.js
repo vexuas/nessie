75e3ad6be79b1e990a7dca13a1102481cd63188a
Gabe R <rodriguezgab1601@gmail.com>
2022-02-08 23:36:10 +0800

    Create table

--- migration.js
+++ migration.js
@@ -3,8 +3,18 @@ const { databaseConfig } = require('./config/database');
 
 const pool = new Pool(databaseConfig);
 
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err);
-  console.log(res);
-  pool.end();
-});
+exports.runMigration = (guilds) => {
+  pool.connect((err, client, done) => {
+    client.query('BEGIN', (err) => {
+      const createTable =
+        'CREATE TABLE IF NOT EXISTS Guild(uuid TEXT NOT NULL PRIMARY KEY, name TEXT NOT NULL, member_count INTEGER NOT NULL, owner_id TEXT NOT NULL, prefix TEXT NOT NULL, use_prefix BOOLEAN NOT NULL DEFAULT TRUE)';
+
+      const insertNewGuild =
+        'INSERT INTO Guild (uuid, name, member_count, owner_id, prefix, use_prefix) VALUES ($uuid, $name, $member_count, $owner_id, $prefix, $use_prefix)';
+      client.query(createTable);
+      guilds.forEach((guild) => {
+        client.query(insertNewGuild);
+      });
+    });
+  });
+};
