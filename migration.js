37b06d43b99c9b6942e2fd37217a648473b3ca2e
Gabe R <rodriguezgab1601@gmail.com>
2022-02-09 00:12:53 +0800

    Update new guilds to not use prefix commands

--- migration.js
+++ migration.js
@@ -9,26 +9,39 @@ exports.runMigration = (guilds) => {
     client.query('BEGIN', (err) => {
       const createTable =
         'CREATE TABLE IF NOT EXISTS Guild(uuid TEXT NOT NULL PRIMARY KEY, name TEXT NOT NULL, member_count INTEGER NOT NULL, owner_id TEXT NOT NULL, prefix TEXT NOT NULL, use_prefix BOOLEAN NOT NULL DEFAULT TRUE)';
-
+      const selectGuilds = 'SELECT * FROM Guild';
       const insertNewGuild =
         'INSERT INTO Guild (uuid, name, member_count, owner_id, prefix, use_prefix) VALUES ($1, $2, $3, $4, $5, $6)';
-
+      const updateUsePrefix = 'UPDATE Guild SET use_prefix = ($1) WHERE uuid = ($2)';
       client.query(createTable);
-      guilds.forEach((guild) => {
-        const insertGuildValues = [
-          guild.id,
-          guild.name,
-          guild.memberCount,
-          guild.ownerId,
-          defaultPrefix,
-          true,
-        ];
-        client.query(insertNewGuild, insertGuildValues, (err, res) => {
-          console.log(err);
-          console.log(res);
-        });
+      // client.query(updateUsePrefix, [false, '491143168352452608'], (err, res) => {
+      //   console.log(err);
+      //   console.log(res);
+      // });
+      // guilds.forEach((guild) => {
+      //   const insertGuildValues = [
+      //     guild.id,
+      //     guild.name,
+      //     guild.memberCount,
+      //     guild.ownerId,
+      //     defaultPrefix,
+      //     true,
+      //   ];
+      //   client.query(insertNewGuild, insertGuildValues, (err, res) => {
+      //     console.log(err);
+      //     console.log(res);
+      //   });
+      // });
+      // client.query('COMMIT', (err) => {
+      //   console.log(err);
+      //   done();
+      // });
+      client.query(selectGuilds, (err, res) => {
+        console.log(res.rows);
+        done();
       });
-      done();
     });
   });
 };
+
+this.runMigration();
