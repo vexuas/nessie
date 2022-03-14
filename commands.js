5f83b3a67f2e33c7bbc1c0cbbbd8ccfa7721aaa2
Gabriel R <42207245+vexuas@users.noreply.github.com>
2022-03-15 00:54:55 +0800

    Delete control command (#50)
    * Comment out status command import
    
    * Delete control commands

--- commands.js
+++ commands.js
@@ -11,8 +11,7 @@ exports.getPrefixCommands = () => {
     //Maps
     br: require('./commands/maps/battle-royale'),
     arenas: require('./commands/maps/arenas'),
-    control: require('./commands/maps/control'),
-    status: require('./commands/maps/status'),
+    // status: require('./commands/maps/status'),
     //Hub
     about: require('./commands/hub/about'),
     help: require('./commands/hub/help'),
@@ -31,6 +30,5 @@ exports.getApplicationCommands = () => {
     // Maps
     br: require('./commands/maps/_battle-royale'),
     arenas: require('./commands/maps/_arenas'),
-    control: require('./commands/maps/_control'),
   };
 };
