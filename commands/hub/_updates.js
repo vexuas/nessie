91cba2979ba6d1c34e5ac2dd1c4214e43acd1d16
Gabe R <rodriguezgab1601@gmail.com>
2022-03-22 01:06:41 +0800

    Update nessie logo url

--- commands/hub/_updates.js
+++ commands/hub/_updates.js
@@ -1,11 +1,12 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { nessieLogo } = require('../../constants');
 
 const sendUpdatesEmbed = async ({ interaction }) => {
   const embed = {
     title: 'Updates | 22 March 2022',
     color: 3447003,
     thumbnail: {
-      url: 'https://cdn.discordapp.com/attachments/889134541615292459/896698383593517066/sir_nessie.png',
+      url: nessieLogo,
     },
   };
   return await interaction.reply({ embeds: [embed] });
