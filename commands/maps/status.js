8430f0d6d36d0f24eb94df7d86c8d166bbfee737
Gabe R <rodriguezgab1601@gmail.com>
2022-05-10 00:16:56 +0800

    Create status start interaction reply

--- commands/maps/status.js
+++ commands/maps/status.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton } = require('discord.js');
 const { getBattleRoyalePubs, getBattleRoyaleRanked } = require('../../adapters');
 const { nessieLogo } = require('../../constants');
 const Scheduler = require('../../scheduler');
@@ -38,6 +39,29 @@ const generateHelpEmbed = () => {
     color: 3447003,
   };
   return [embedData];
+};
+const sendStartInteraction = async (interaction) => {
+  const embedData = {
+    title: 'Status | Start',
+    color: 3447003,
+    description:
+      'By confirming below, Nessie will create a new category channel and 2 new text channels for the automated map status:\n• `Apex Map Status`\n• `#apex-pubs`\n• `#apex-ranked`\n\nNessie will use these channels to send automatic updates every 15 minutes',
+  };
+  const row = new MessageActionRow()
+    .addComponents(
+      new MessageButton()
+        .setCustomId('statusStart__cancelButton')
+        .setLabel('Cancel')
+        .setStyle('SECONDARY')
+    )
+    .addComponents(
+      new MessageButton()
+        .setCustomId('statusStart__startButton')
+        .setLabel(`Let's go!`)
+        .setStyle('SUCCESS')
+    );
+
+  await interaction.editReply({ components: [row], embeds: [embedData] });
 };
 const generatePubsEmbed = (data) => {
   const embedData = {
@@ -111,29 +135,30 @@ module.exports = {
           await interaction.editReply({ embeds: embedToSend });
           break;
         case 'start':
-          const pubsData = await getBattleRoyalePubs();
-          const rankedData = await getBattleRoyaleRanked();
-          const pubsEmbed = generatePubsEmbed(pubsData);
-          const rankedEmbed = generateRankedEmbed(rankedData);
-          // //Creates a category channel for better readability
-          const statusCategory = await interaction.guild.channels.create(
-            'Apex Legends Map Status',
-            {
-              type: 'GUILD_CATEGORY',
-            }
-          );
-          //Creates the status channnel for br
-          const statusPubsChannel = await interaction.guild.channels.create('apex-pubs', {
-            parent: statusCategory,
-          });
-          const statusRankedChannel = await interaction.guild.channels.create('apex-ranked', {
-            parent: statusCategory,
-          });
-          const statusPubsMessage = await statusPubsChannel.send({ embeds: pubsEmbed }); //Sends initial br embed in status channel
-          const statusRankedMessage = await statusRankedChannel.send({ embeds: rankedEmbed });
-          await interaction.editReply(
-            `Created map status at ${statusPubsChannel} and ${statusRankedChannel}`
-          ); //Sends success message in channel where command got instantiated
+          await sendStartInteraction(interaction);
+          // const pubsData = await getBattleRoyalePubs();
+          // const rankedData = await getBattleRoyaleRanked();
+          // const pubsEmbed = generatePubsEmbed(pubsData);
+          // const rankedEmbed = generateRankedEmbed(rankedData);
+          // // //Creates a category channel for better readability
+          // const statusCategory = await interaction.guild.channels.create(
+          //   'Apex Legends Map Status',
+          //   {
+          //     type: 'GUILD_CATEGORY',
+          //   }
+          // );
+          // //Creates the status channnel for br
+          // const statusPubsChannel = await interaction.guild.channels.create('apex-pubs', {
+          //   parent: statusCategory,
+          // });
+          // const statusRankedChannel = await interaction.guild.channels.create('apex-ranked', {
+          //   parent: statusCategory,
+          // });
+          // const statusPubsMessage = await statusPubsChannel.send({ embeds: pubsEmbed }); //Sends initial br embed in status channel
+          // const statusRankedMessage = await statusRankedChannel.send({ embeds: rankedEmbed });
+          // await interaction.editReply(
+          //   `Created map status at ${statusPubsChannel} and ${statusRankedChannel}`
+          // ); //Sends success message in channel where command got instantiated
           break;
         case 'stop':
           await interaction.editReply('Status Stop Command');
