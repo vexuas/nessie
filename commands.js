bc8698220a4995ff203343cfc4e7f70126b9375e
Gabriel R <42207245+vexuas@users.noreply.github.com>
2022-06-01 00:32:27 +0800

    Spiking dem rate limits (#69)

--- commands.js
+++ commands.js
@@ -10,6 +10,7 @@ exports.getApplicationCommands = () => {
     help: require('./commands/hub/help'),
     invite: require('./commands/hub/invite'),
     updates: require('./commands/hub/updates'),
+    // ratelimit: require('./commands/hub/rateLimit'),
     // Maps
     br: require('./commands/maps/battle-royale'),
     arenas: require('./commands/maps/arenas'),
