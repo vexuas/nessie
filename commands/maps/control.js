0e79b256edf5d5c121eb29f5103d4fb9dff02527
Gabriel R <42207245+vexuas@users.noreply.github.com>
2022-06-23 00:24:49 +0800

    Add error alert for control (#78)
    * Add special error handling for control
    
    * Add comments

--- commands/maps/control.js
+++ commands/maps/control.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { getControlPubs } = require('../../adapters');
-const { sendErrorLog, generateErrorEmbed, generatePubsEmbed } = require('../../helpers');
+const { sendErrorLog, generateErrorEmbed, generatePubsEmbed, codeBlock } = require('../../helpers');
 const { v4: uuidv4 } = require('uuid');
 const { sendMixpanelEvent } = require('../../analytics');
 
@@ -38,10 +38,31 @@ module.exports = {
         true
       );
     } catch (error) {
+      /**
+       * Special error handling here instead of using the error helper
+       * This is due to control being a limited time event and it keeps being taken on and off the game mode list
+       * I've been removing the command and then readding it back in a new release so it's been quite a hassle to actually maintain this
+       * Decided to always just keep it in and show an alert error message when it does fail
+       * Fetches the alert in the alert channel in the support server
+       */
       const uuid = uuidv4();
+      const alertChannel = nessie.channels.cache.get('973977422699573258');
+      const messageObject = await alertChannel.messages.fetch('989197310502260736');
+      const errorMessage = messageObject.content;
+      const errorAlert = errorMessage.substring(
+        errorMessage.indexOf('[') + 4,
+        errorMessage.lastIndexOf(']') - 3
+      );
+      const errorEmbed = {
+        description: `${errorAlert}\n\nError: ${
+          error.message ? codeBlock(error.message) : codeBlock('Unexpected Error')
+        }\nError ID: ${codeBlock(
+          uuid
+        )}\nAlternatively, you can also report issue through the [support server](https://discord.gg/FyxVrAbRAd)`,
+        color: 16711680,
+      };
       const type = 'Control';
-      const errorEmbed = await generateErrorEmbed(error, uuid, nessie);
-      await interaction.editReply({ embeds: errorEmbed });
+      await interaction.editReply({ embeds: [errorEmbed] });
       await sendErrorLog({ nessie, error, interaction, type, uuid });
     }
   },
