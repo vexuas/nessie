9ade2a79eda489af9fcec55d06e21cabe6f66db1
Gabe R <rodriguezgab1601@gmail.com>
2022-06-23 23:21:22 +0800

    Send br content

--- commands/admin/webhook.js
+++ commands/admin/webhook.js
@@ -1,6 +1,9 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { WebhookClient } = require('discord.js');
+const { getBattleRoyalePubs } = require('../../adapters');
 const { testWebhook } = require('../../config/nessie.json');
+const { generatePubsEmbed } = require('../../helpers');
+const { nessieLogo } = require('../../constants');
 
 module.exports = {
   isAdmin: true,
@@ -10,9 +13,16 @@ module.exports = {
       id: testWebhook.id,
       token: testWebhook.token,
     });
+
     try {
       await interaction.deferReply();
-      await webhook.send({ content: 'test' });
+      const data = await getBattleRoyalePubs();
+      const embed = generatePubsEmbed(data);
+      await webhook.send({
+        username: 'Nessie Map Status',
+        avatarURL: nessieLogo,
+        embeds: [embed],
+      });
       await interaction.editReply({ content: 'Sent Webhook' });
     } catch (error) {}
   },
