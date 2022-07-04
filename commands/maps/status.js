084be961567acf4e72e535764f4789223b99ca6b
Gabe R <rodriguezgab1601@gmail.com>
2022-07-05 00:51:29 +0800

    Check if bot has required permissions

--- commands/maps/status.js
+++ commands/maps/status.js
@@ -3,28 +3,31 @@ const { MessageActionRow, MessageButton } = require('discord.js');
 const { generateErrorEmbed, sendErrorLog } = require('../../helpers');
 const { v4: uuidv4 } = require('uuid');
 
-/**
- * Temporary handler for status command
- * Just displays information on why there's no proper status yet
- * Also has an invite button to redirect people to the support server
- * TODO: Probably need to make the support server an actual community server with actual rules and stuff
- */
 const sendHelpInteraction = async ({ interaction, nessie }) => {
+  console.log(interaction);
+  const hasManageChannels = interaction.guild.me.permissions.has('MANAGE_CHANNELS', false);
+  const hasManageWebhooks = interaction.guild.me.permissions.has('MANAGE_WEBHOOKS', false);
+  const hasSendMessages = interaction.guild.me.permissions.has('SEND_MESSAGES', false);
   try {
     const embedData = {
-      title: 'Status | About',
-      description:
-        "Due to technical limitations with Discord, automatic updates through normal messages/interactions isn't possible in a large scale. Fortunately an alternative approach is being worked on but it might take a while before it gets released\n\nAs a temporary solution, I've set up automatic map updates in announcement channels in the support server.\n\nFeel free to join and follow the channels!",
+      title: 'Status | Help',
+      description: 'Something here',
+      fields: [
+        {
+          name: 'User Permissions',
+          value: '• Administrator',
+        },
+        {
+          name: 'Bot Permissions',
+          value: `${hasManageChannels ? '✅ ' : '❌'} Manage Channels\n${
+            hasManageWebhooks ? '✅ ' : '❌'
+          } Manage Webhooks\n${hasSendMessages ? '✅ ' : '❌'} Send Messages`,
+        },
+      ],
       color: 3447003,
     };
-    const row = new MessageActionRow().addComponents(
-      new MessageButton()
-        .setLabel("To Nessie's Canyon")
-        .setStyle('LINK')
-        .setURL('https://discord.gg/FyxVrAbRAd')
-    );
 
-    return await interaction.editReply({ components: [row], embeds: [embedData] });
+    return await interaction.editReply({ embeds: [embedData] });
   } catch (error) {
     const uuid = uuidv4();
     const type = 'Status About';
@@ -55,7 +58,7 @@ module.exports = {
       await interaction.deferReply();
       switch (statusOption) {
         case 'help':
-          return interaction.editReply('Selected status help');
+          return sendHelpInteraction({ interaction, nessie });
         case 'start':
           return interaction.editReply('Selected status start');
         case 'stop':
