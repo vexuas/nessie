90641df5c3ac0df004e887a18acf091855461b14
Gabriel R <42207245+vexuas@users.noreply.github.com>
2022-07-18 01:32:25 +0800

    Update status copies (#103)
    * Update embed content of status help
    
    * Add view channel to required bot permissions
    
    * Add new invite link with updated permissions in status help
    
    * Update status confirm embed content
    
    * Update about embed content
    
    * Update invite links

--- commands/hub/invite/index.js
+++ commands/hub/invite/index.js
@@ -6,7 +6,7 @@ module.exports = {
     .setDescription('Generates an invite link for Nessie'),
   async execute({ interaction }) {
     const embed = {
-      description: `<@${interaction.user.id}> | [Add me to your servers! (◕ᴗ◕✿)](https://discord.com/api/oauth2/authorize?client_id=889135055430111252&permissions=3088&scope=applications.commands%20bot)`,
+      description: `<@${interaction.user.id}> | [Add me to your servers! (◕ᴗ◕✿)](https://discord.com/api/oauth2/authorize?client_id=889135055430111252&permissions=536874000&scope=applications.commands%20bot)`,
       color: 3447003,
     };
     return await interaction.reply({ embeds: [embed] });
--- commands/maps/status/help/index.js
+++ commands/maps/status/help/index.js
@@ -4,6 +4,7 @@ const {
   sendErrorLog,
   checkMissingBotPermissions,
   checkIfAdminUser,
+  codeBlock,
 } = require('../../../../helpers');
 
 /**
@@ -14,15 +15,29 @@ const {
  * Shows a success/warning at the end if any of the permissions are missing
  */
 const sendHelpInteraction = async ({ interaction, nessie }) => {
-  const { hasAdmin, hasManageChannels, hasManageWebhooks, hasSendMessages, hasMissingPermissions } =
-    checkMissingBotPermissions(interaction);
+  const {
+    hasAdmin,
+    hasManageChannels,
+    hasManageWebhooks,
+    hasViewChannel,
+    hasSendMessages,
+    hasMissingPermissions,
+  } = checkMissingBotPermissions(interaction);
   const isAdminUser = checkIfAdminUser(interaction);
 
   try {
-    const embedData = {
-      title: 'Status | Help',
-      description:
-        "• Explain the status command does\n• Explain what it'll create; channels, webhooks\n• Explain necessary user permissions; admin\n• Explain bot permissions; whatever nessie needs to operate",
+    const embedInformation = {
+      title: 'Information',
+      description: `This command will send automatic updates of Apex Legends Map Rotations. You will be able to choose which game modes, *Battle Royale or/and Arenas*, to get updates for both pubs and ranked.\n\nDepending on what you choose, Nessie will create a set of:\n• ${codeBlock(
+        'Category Channel'
+      )}\n• ${codeBlock('Text Channel(s)')}\n• ${codeBlock(
+        'Webhook(s)'
+      )}\nUpdates will then be sent in these channels **every 15 minutes**\n\n`,
+      color: 3447003,
+    };
+    const embedPermissions = {
+      title: 'Permissions',
+      description: `Nessie requires certain permissions to properly create automatic updates. See the checklist below for the full list.`,
       fields: [
         {
           name: 'User Permissions',
@@ -32,9 +47,15 @@ const sendHelpInteraction = async ({ interaction, nessie }) => {
           name: 'Bot Permissions',
           value: `${hasAdmin || hasManageChannels ? '✅' : '❌'} Manage Channels\n${
             hasAdmin || hasManageWebhooks ? '✅' : '❌'
-          } Manage Webhooks\n${hasAdmin || hasSendMessages ? '✅' : '❌'} Send Messages\n\n${
+          } Manage Webhooks\n${hasAdmin || hasViewChannel ? '✅' : '❌'} View Channels\n${
+            hasAdmin || hasSendMessages ? '✅' : '❌'
+          } Send Messages\n\n${
             !isAdminUser || hasMissingPermissions
-              ? 'Looks like there are missing permissions. Make sure to add the above permissions to be able to create automatic map updates!'
+              ? `Looks like there are missing permissions. Make sure to add the above permissions to be able to use automatic map updates!${
+                  isAdminUser
+                    ? `\nYou can refresh Nessie's permissions by reinviting using this [link](https://discord.com/api/oauth2/authorize?client_id=889135055430111252&permissions=536874000&scope=applications.commands%20bot)`
+                    : ''
+                }`
               : 'Looks like everything is set, use `/status start` to get started!'
           }`,
         },
@@ -42,7 +63,13 @@ const sendHelpInteraction = async ({ interaction, nessie }) => {
       color: 3447003,
     };
 
-    return await interaction.editReply({ embeds: [embedData] });
+    return await interaction.editReply({
+      embeds: [
+        { title: 'Status | Help', description: '', color: 3447003 },
+        embedInformation,
+        embedPermissions,
+      ],
+    });
   } catch (error) {
     const uuid = uuidv4();
     const type = 'Status Help';
