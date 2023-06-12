8752c7c4be6479a9a43d93f6def1706913ce97e7
Gab <rodriguezgab1601@gmail.com>
2023-06-12 22:10:13 +0800

    Update commands import

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -6,17 +6,16 @@
 exports.getApplicationCommands = () => {
   return {
     //Hub
-    about: require('./commands/hub/about'),
-    help: require('./commands/hub/help'),
-    invite: require('./commands/hub/invite'),
-    updates: require('./commands/hub/updates'),
+    about: require('./hub/about'),
+    help: require('./hub/help'),
+    invite: require('./hub/invite'),
+    updates: require('./hub/updates'),
     // Maps
-    br: require('./commands/maps/battleRoyale'),
-    arenas: require('./commands/maps/arenas'),
-    // control: require('./commands/maps/control'),
-    ltm: require('./commands/maps/ltm'),
-    status: require('./commands/maps/status'),
+    br: require('./maps/battleRoyale'),
+    arenas: require('./maps/arenas'),
+    ltm: require('./maps/ltm'),
+    status: require('./maps/status'),
     //Admin
-    announcement: require('./commands/admin/announcement'),
+    announcement: require('./admin/announcement'),
   };
 };
