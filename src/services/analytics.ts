e761007c289e8824a6489d05c11344bf904de218
Gab <rodriguezgab1601@gmail.com>
2023-06-13 21:47:00 +0800

    Convert analytics and install lodash

---
+++ src/services/analytics.ts
@@ -0,0 +1,62 @@
+import { Guild, GuildTextBasedChannel, User } from 'discord.js';
+import { Mixpanel } from 'mixpanel';
+import { capitalize } from 'lodash';
+
+//TODO: Check if sub commands are being tracked correctly
+type CommandEvent = {
+  client: Mixpanel;
+  user: User;
+  channel: GuildTextBasedChannel | null;
+  guild: Guild | null;
+  command: string;
+  options?: string;
+  properties?: object;
+};
+type UserProfile = {
+  client: Mixpanel;
+  user: User;
+  channel: GuildTextBasedChannel | null;
+  guild: Guild | null;
+  command?: string;
+};
+
+function setUserProfile({ client, user, channel, guild, command }: UserProfile) {
+  client.people.set(user.id, {
+    $name: user.username,
+    $created: user.createdAt.toISOString(),
+    tag: user.tag,
+    guild: guild ? guild.name : 'N/A',
+    guild_id: guild ? guild.id : 'N/A',
+  });
+  client.people.set_once(user.id, {
+    first_used: new Date().toISOString(),
+    first_command: command,
+    first_used_in_guild: guild ? guild.name : 'N/A',
+    first_used_in_channel: channel ? channel.name : 'N/A',
+  });
+}
+export function sendCommandEvent({
+  user,
+  channel,
+  guild,
+  command,
+  client,
+  options,
+  properties,
+}: CommandEvent) {
+  setUserProfile({ client, user, guild, channel, command });
+  const eventName = `Use ${capitalize(command)} Command`;
+
+  client.track(eventName, {
+    distinct_id: user.id,
+    user: user.tag,
+    user_name: user.username,
+    channel: channel ? channel.name : 'N/A',
+    channel_id: channel ? channel.id : 'N/A',
+    guild: guild ? guild.name : 'N/A',
+    guild_id: guild ? guild.id : 'N/A',
+    command: command,
+    arguments: options ? options : 'none',
+    ...properties,
+  });
+}
