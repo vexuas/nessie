e051702c0135443adc7ecfb4885eeff6e86724a3
Gab <rodriguezgab1601@gmail.com>
2023-06-14 20:32:38 +0800

    Convert invite

---
+++ src/commands/invite/index.ts
@@ -0,0 +1,27 @@
+import { AppCommand, AppCommandOptions } from '../commands';
+import { sendErrorLog } from '../../utils/helpers';
+import { hyperlink, SlashCommandBuilder } from '@discordjs/builders';
+
+//TODO: Add typing after upgrading to djs v14
+export const generateInviteEmbed = () => {
+  const embed = {
+    description: hyperlink('Add me to your servers! (◕ᴗ◕✿)', ''),
+    color: 3447003,
+  };
+  return embed;
+};
+
+export default {
+  commandType: 'Information',
+  data: new SlashCommandBuilder()
+    .setName('invite')
+    .setDescription('Generates an invite link for My App'),
+  async execute({ interaction }: AppCommandOptions) {
+    try {
+      const embed = generateInviteEmbed();
+      await interaction.reply({ embeds: [embed] });
+    } catch (error) {
+      sendErrorLog({ error, interaction });
+    }
+  },
+} as AppCommand;
