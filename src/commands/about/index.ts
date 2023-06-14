09c3fc87778c8152698cde3140999c2902463f83
Gab <rodriguezgab1601@gmail.com>
2023-06-14 20:46:01 +0800

    Create script for bot versioning and date update

--- src/commands/about/index.ts
+++ src/commands/about/index.ts
@@ -1,15 +1,16 @@
 import { BOT_UPDATED_AT, BOT_VERSION } from '../../version';
 import { AppCommand, AppCommandOptions } from '../commands';
 import { format } from 'date-fns';
-import { getEmbedColor, sendErrorLog } from '../../utils/helpers';
+import { sendErrorLog } from '../../utils/helpers';
 import { Client } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
 
+//TODO: Add typing after upgrading to djs v14
 export const generateAboutEmbed = (app?: Client) => {
   const embed = {
     title: 'Info',
     description: `Hi there! I’m Nessie and I provide an easy way to get status updates of Map Rotations in Apex Legends! Hope that you can find me useful (◕ᴗ◕✿)\n\nTry out my new beta feature: **Automatic Map Updates**! To check out what it is, use \`/status help\`!\n\nAll my data is extracted from the great works of [https://apexlegendsapi.com/](https://apexlegendsapi.com/). Go support them too, it’s a cool project!\n\nFor the latest news, check out \`/updates\`!`,
-    color: getEmbedColor(),
+    color: 3447003,
     thumbnail: {
       url: 'https://cdn.discordapp.com/attachments/1089616880576245853/1094559253395689562/mitsuha.jpg',
     },
