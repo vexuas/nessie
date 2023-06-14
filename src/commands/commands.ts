9126719ce3875ca4467155764f449714476b4423
Gab <rodriguezgab1601@gmail.com>
2023-06-14 20:24:00 +0800

    Refactor main commands file

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,21 +1,38 @@
-/**
- * Command list of nessie
- * Creating this extra file for easier readability of commands
- * TODO: Instead of importing individually, auto import whatever is under the commands folder
- */
-export const getApplicationCommands = () => {
-  return {
-    //Hub
-    about: require('./hub/about'),
-    help: require('./hub/help'),
-    invite: require('./hub/invite'),
-    updates: require('./hub/updates'),
-    // Maps
-    br: require('./maps/battleRoyale'),
-    arenas: require('./maps/arenas'),
-    ltm: require('./maps/ltm'),
-    status: require('./maps/status'),
-    //Admin
-    announcement: require('./admin/announcement'),
-  };
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { Client, CommandInteraction } from 'discord.js';
+import fs from 'fs';
+import path from 'path';
+
+export type AppCommand = {
+  data: SlashCommandBuilder;
+  commandType?: string;
+  execute: (data: AppCommandOptions) => Promise<void>;
 };
+export type AppCommandOptions = {
+  interaction: CommandInteraction;
+  app: Client;
+  appCommands?: AppCommand[];
+};
+type ExportedAppCommand = {
+  default: AppCommand;
+};
+export function getApplicationCommands(): AppCommand[] {
+  const appCommands: AppCommand[] = [];
+
+  const loadModules = (directoryPath: string) => {
+    const files = fs.readdirSync(directoryPath, { withFileTypes: true });
+    files.forEach((file) => {
+      const filePath = path.join(directoryPath, file.name);
+      if (file.isDirectory()) {
+        return loadModules(filePath);
+      }
+      if (file.name === 'index.js') {
+        const modulePath = `./${filePath.replace('dist/commands/', '')}`;
+        const currentModule = require(modulePath) as ExportedAppCommand;
+        appCommands.push(currentModule.default);
+      }
+    });
+  };
+  loadModules('./dist/commands');
+  return appCommands;
+}
