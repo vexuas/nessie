37dd025ef0181ab9ec42ac7a98ded1f39dd3f39a
Gab <rodriguezgab1601@gmail.com>
2023-06-15 14:57:56 +0800

    Add notificaiton to guild listeners

--- src/events/guildCreate/index.ts
+++ src/events/guildCreate/index.ts
@@ -1,10 +1,24 @@
 import { insertNewGuild } from '../../services/database';
 import { EventModule } from '../events';
+import { isEmpty } from 'lodash';
+import { Guild, WebhookClient } from 'discord.js';
+import { DATABASE_CONFIG, GUILD_NOTIFICATION_WEBHOOK_URL } from '../../config/environment';
+import { serverNotificationEmbed } from '../../utils/helpers';
+import { nessieLogo } from '../../utils/constants';
 
 export default function ({ nessie }: EventModule) {
-  nessie.on('guildCreate', (guild) => {
+  nessie.on('guildCreate', async (guild: Guild) => {
     try {
-      insertNewGuild(guild);
+      DATABASE_CONFIG && (await insertNewGuild(guild));
+      if (GUILD_NOTIFICATION_WEBHOOK_URL && !isEmpty(GUILD_NOTIFICATION_WEBHOOK_URL)) {
+        const embed = await serverNotificationEmbed({ app: nessie, guild, type: 'join' });
+        const notificationWebhook = new WebhookClient({ url: GUILD_NOTIFICATION_WEBHOOK_URL });
+        await notificationWebhook.send({
+          embeds: [embed],
+          username: 'Nessie Server Notification',
+          avatarURL: nessieLogo,
+        });
+      }
     } catch (e) {
       console.log(e); // Add proper handling
     }
--- src/events/guildDelete/index.ts
+++ src/events/guildDelete/index.ts
@@ -1,10 +1,24 @@
+import { DATABASE_CONFIG, GUILD_NOTIFICATION_WEBHOOK_URL } from '../../config/environment';
 import { deleteGuild } from '../../services/database';
 import { EventModule } from '../events';
+import { isEmpty } from 'lodash';
+import { serverNotificationEmbed } from '../../utils/helpers';
+import { Guild, WebhookClient } from 'discord.js';
+import { nessieLogo } from '../../utils/constants';
 
 export default function ({ nessie }: EventModule) {
-  nessie.on('guildDelete', (guild) => {
+  nessie.on('guildDelete', async (guild: Guild) => {
     try {
-      deleteGuild(guild);
+      DATABASE_CONFIG && (await deleteGuild(guild));
+      if (GUILD_NOTIFICATION_WEBHOOK_URL && !isEmpty(GUILD_NOTIFICATION_WEBHOOK_URL)) {
+        const embed = await serverNotificationEmbed({ app: nessie, guild, type: 'leave' });
+        const notificationWebhook = new WebhookClient({ url: GUILD_NOTIFICATION_WEBHOOK_URL });
+        await notificationWebhook.send({
+          embeds: [embed],
+          username: 'Nessie Server Notification',
+          avatarURL: nessieLogo,
+        });
+      }
     } catch (e) {
       console.log(e); // Add proper handling
     }
