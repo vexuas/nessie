a65be1f660e8d4151b096982eb5bf9c87adc289b
Gab <rodriguezgab1601@gmail.com>
2023-06-18 19:15:20 +0800

    Use got instead of axios for rotation data

--- src/services/adapters.ts
+++ src/services/adapters.ts
@@ -1,16 +1,18 @@
 import axios from 'axios';
+import got from 'got';
 import { ALS_API_KEY } from '../config/environment';
 
 //Documentation on API: https://apexlegendsapi.com/documentation.php
 const url = `https://api.mozambiquehe.re/maprotation?version=2&auth=${ALS_API_KEY}`;
 
 //TODO: Add schema for ALS responses
+export async function getRotationData(): Promise<any> {
+  const response: string = (await got.get(url)).body;
+  return JSON.parse(response);
+}
 export const getBattleRoyalePubs = async () => {
-  const data = await axios.get(url).then((response: any) => {
-    return response.data.battle_royale;
-  });
-  console.log(data);
-  return data;
+  const response = await getRotationData();
+  return response.battle_royale;
 };
 export const getBattleRoyaleRanked = async () => {
   const data = await axios.get(url).then((response: any) => {
@@ -43,13 +45,6 @@ export const getControlPubs = async () => {
 export const getLimitedTimeEvent = async () => {
   const data = await axios.get(url).then((response: any) => {
     return response.data.ltm;
-  });
-  console.log(data);
-  return data;
-};
-export const getRotationData = async () => {
-  const data = await axios.get(url).then((response: any) => {
-    return response.data;
   });
   console.log(data);
   return data;
