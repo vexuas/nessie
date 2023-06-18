67acf1aacacba5f2793467db6634ecc3056d7230
Gab <rodriguezgab1601@gmail.com>
2023-06-18 19:39:40 +0800

    Split schemas between modes

--- src/schemas/mapRotation.ts
+++ src/schemas/mapRotation.ts
@@ -1,141 +1,60 @@
-export interface ALSMapRotationAPIObject {
-  battle_royale: {
-    current: {
-      start: number;
-      end: number;
-      readableDate_start: string;
-      readableDate_end: string;
-      map: string;
-      code: string;
-      DurationInSecs: number;
-      DurationInMinutes: number;
-      asset: string;
-      remainingSecs: number;
-      remainingMins: number;
-      remainingTimer: string;
-    };
-    next: {
-      start: number;
-      end: number;
-      readableDate_start: string;
-      readableDate_end: string;
-      map: string;
-      code: string;
-      DurationInSecs: number;
-      DurationInMinutes: number;
-      asset: string;
-    };
-  };
-  arenas: {
-    current: {
-      start: number;
-      end: number;
-      readableDate_start: string;
-      readableDate_end: string;
-      map: string;
-      code: string;
-      DurationInSecs: number;
-      DurationInMinutes: number;
-      asset: string;
-      remainingSecs: number;
-      remainingMins: number;
-      remainingTimer: string;
-    };
-    next: {
-      start: number;
-      end: number;
-      readableDate_start: string;
-      readableDate_end: string;
-      map: string;
-      code: string;
-      DurationInSecs: number;
-      DurationInMinutes: number;
-      asset: string;
-    };
-  };
-  ranked: {
-    current: {
-      start: number;
-      end: number;
-      readableDate_start: string;
-      readableDate_end: string;
-      map: string;
-      code: string;
-      DurationInSecs: number;
-      DurationInMinutes: number;
-      asset: string;
-      remainingSecs: number;
-      remainingMins: number;
-      remainingTimer: string;
-    };
-    next: {
-      start: number;
-      end: number;
-      readableDate_start: string;
-      readableDate_end: string;
-      map: string;
-      code: string;
-      DurationInSecs: number;
-      DurationInMinutes: number;
-      asset: string;
-    };
-  };
-  arenasRanked: {
-    current: {
-      start: number;
-      end: number;
-      readableDate_start: string;
-      readableDate_end: string;
-      map: string;
-      code: string;
-      DurationInSecs: number;
-      DurationInMinutes: number;
-      asset: string;
-      remainingSecs: number;
-      remainingMins: number;
-      remainingTimer: string;
-    };
-    next: {
-      start: number;
-      end: number;
-      readableDate_start: string;
-      readableDate_end: string;
-      map: string;
-      code: string;
-      DurationInSecs: number;
-      DurationInMinutes: number;
-      asset: string;
-    };
-  };
-  ltm: {
-    current: {
-      start: number;
-      end: number;
-      readableDate_start: string;
-      readableDate_end: string;
-      map: string;
-      code: string;
-      DurationInSecs: number;
-      DurationInMinutes: number;
-      isActive: boolean;
-      eventName: string;
-      asset: string;
-      remainingSecs: number;
-      remainingMins: number;
-      remainingTimer: string;
-    };
-    next: {
-      start: number;
-      end: number;
-      readableDate_start: string;
-      readableDate_end: string;
-      map: string;
-      code: string;
-      DurationInSecs: number;
-      DurationInMinutes: number;
-      isActive: boolean;
-      eventName: string;
-      asset: string;
-    };
-  };
+export interface MapRotationAPIObject {
+  battle_royale: MapRotationBattleRoyaleSchema;
+  arenas: MapRotationArenasSchema;
+  ranked: MapRotationRankedSchema;
+  arenasRanked: MapRotationArenasRankedSchema;
+  ltm: MapRotationLimitedTimeMapSchema;
+}
+interface MapRotationCurrentSchema {
+  start: number;
+  end: number;
+  readableDate_start: string;
+  readableDate_end: string;
+  map: string;
+  code: string;
+  DurationInSecs: number;
+  DurationInMinutes: number;
+  asset: string;
+  remainingSecs: number;
+  remainingMins: number;
+  remainingTimer: string;
+}
+interface MapRotationNextSchema {
+  start: number;
+  end: number;
+  readableDate_start: string;
+  readableDate_end: string;
+  map: string;
+  code: string;
+  DurationInSecs: number;
+  DurationInMinutes: number;
+  asset: string;
+}
+interface MapRotationLimitedTimeMapCurrentSchema extends MapRotationCurrentSchema {
+  isActive: boolean;
+  eventName: string;
+}
+interface MapRotationLimitedTimeMapNextSchema extends MapRotationNextSchema {
+  isActive: boolean;
+  eventName: string;
+}
+export interface MapRotationBattleRoyaleSchema {
+  current: MapRotationCurrentSchema;
+  next: MapRotationNextSchema;
+}
+export interface MapRotationArenasSchema {
+  current: MapRotationCurrentSchema;
+  next: MapRotationNextSchema;
+}
+export interface MapRotationRankedSchema {
+  current: MapRotationCurrentSchema;
+  next: MapRotationNextSchema;
+}
+export interface MapRotationArenasRankedSchema {
+  current: MapRotationCurrentSchema;
+  next: MapRotationNextSchema;
+}
+export interface MapRotationLimitedTimeMapSchema {
+  current: MapRotationLimitedTimeMapCurrentSchema;
+  next: MapRotationLimitedTimeMapNextSchema;
 }
