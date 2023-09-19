35fe84b67879b491bdf398b8153403c671ed7b74
Gab <rodriguezgab1601@gmail.com>
2023-09-20 01:24:18 +0800

    Update mixtape embed

--- src/commands/mixtape/index.ts
+++ src/commands/mixtape/index.ts
@@ -1,25 +1,24 @@
 import { APIEmbed, SlashCommandBuilder } from 'discord.js';
 import { MapRotationMixtapeSchema } from '../../schemas/mapRotation';
 import { getMixtape } from '../../services/adapters';
-import { getCountdown, getMapUrl, sendErrorLog } from '../../utils/helpers';
+import { getCountdown, sendErrorLog } from '../../utils/helpers';
 import { AppCommand, AppCommandOptions } from '../commands';
 
 export const generateMixtapeEmbed = (data: MapRotationMixtapeSchema): APIEmbed => {
-  const mapURL = getMapUrl(data.current.code);
   const embedData: APIEmbed = {
-    title: data.current.eventName,
+    title: `Mixtape`,
     color: 15105570,
     image: {
-      url: mapURL && mapURL.length > 0 ? mapURL : data.current.asset,
+      url: data.current.asset,
     },
     timestamp: new Date(Date.now() + data.current.remainingSecs * 1000).toISOString(),
     footer: {
-      text: `Next Map: ${data.next.map}`,
+      text: `Next Mode: ${data.next.eventName} | ${data.next.map}`,
     },
     fields: [
       {
-        name: 'Current map',
-        value: '```fix\n\n' + data.current.map + '```',
+        name: 'Current mode',
+        value: '```fix\n\n' + `${data.current.eventName} | ${data.current.map}` + '```',
         inline: true,
       },
       {
@@ -40,6 +39,7 @@ export default {
     try {
       await interaction.deferReply();
       const data = await getMixtape();
+      console.log(data);
       const embed = generateMixtapeEmbed(data);
       await interaction.editReply({ embeds: [embed] });
     } catch (error) {
