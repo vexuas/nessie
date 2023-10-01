7a67a55a6f718029db40d8ca69e4fe6028c6282d
Gab <rodriguezgab1601@gmail.com>
2023-10-01 23:04:35 +0800

    Add todo for timezones

--- src/commands/battleRoyale/index.ts
+++ src/commands/battleRoyale/index.ts
@@ -38,6 +38,7 @@ export default {
         case 'br_ranked':
           data = await getBattleRoyaleRanked();
           const season = await getSeasonInformation();
+          //TODO: Figure out formatting for different timezones eventually
           const seasonEnd = formatSeasonEndCountdown({
             seasonEnd: season.dates.End * 1000,
             currentDate: new Date(),
