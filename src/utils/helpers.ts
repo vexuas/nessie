fac03e9dbc8cf950d9bd58d87e651f9ea7eb850f
vexuas <rodriguezgab1601@gmail.com>
2024-08-18 23:21:41 +0800

    Add fallback embed if no data is passed

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -323,9 +323,14 @@ export const getCountdown = (timer: string) => {
  * As discord embed has a timestamp propery, I added the remianing milliseconds to the current date
  */
 export const generatePubsEmbed = (
-  data: MapRotationBattleRoyaleSchema | MapRotationArenasSchema,
+  data?: MapRotationBattleRoyaleSchema | MapRotationArenasSchema,
   type = 'Battle Royale'
 ): APIEmbed => {
+  if(!data) return {
+    title: `${type} | Ranked`,
+    color: 16711680,
+    description: 'Oops unavailable to get data. Please try again later!'
+  }
   const embedData: APIEmbed = {
     title: `${type} | Pubs`,
     color: 3066993,
@@ -356,11 +361,16 @@ export const generatePubsEmbed = (
  * Fairly simple, don't need any fancy timers and footers
  */
 export const generateRankedEmbed = (
-  data: MapRotationRankedSchema | MapRotationArenasRankedSchema,
+  data?: MapRotationRankedSchema | MapRotationArenasRankedSchema,
   type = 'Battle Royale',
   seasonEnd?: string | null,
   splitEnd?: string | null
 ) => {
+  if(!data) return {
+    title: `${type} | Ranked`,
+    color: 16711680,
+    description: 'Oops unavailable to get data. Please try again later!'
+  }
   const embedData: any = {
     title: `${type} | Ranked`,
     color: 7419530,
