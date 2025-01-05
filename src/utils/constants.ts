074675f259e371378792290e2ea29691b5326342
vexuas <rodriguezgab1601@gmail.com>
2025-01-05 15:49:04 +0800

    Update nessie logo to use personal cdn

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,1 @@
-export const nessieLogo =
-  'https://cdn.discordapp.com/attachments/248430185463021569/955497690735738900/Nessie_Logo_v2.png'; //Maybe host this somewhere other than discord
+export const nessieLogo = 'https://vexuas.b-cdn.net/Nessie_Logo_v2.png';
