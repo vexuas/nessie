bb3d166f9de1e7b676fd4569028dd7d23393d262
vexuas <rodriguezgab1601@gmail.com>
2025-04-14 02:28:14 +0800

    Add comment

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -1,15 +1,18 @@
 import { createClient } from 'redis';
 
+// Using default url for Redis server
+// I use VPS and have Redis running on the same server so it's the same as developing in local
+// Replace this with your Redis server URL if it's different
 const redis = createClient({
   url: 'redis://localhost:6379',
 });
 
-redis.on('error', (err) => console.error('❌ Redis error:', err));
+redis.on('error', (err) => console.error('Redis Client Error', err));
 
 export const connectRedis = async () => {
   if (!redis.isOpen) {
     await redis.connect();
-    console.log('✅ Connected to Redis');
+    console.log('Connected to Redis');
   }
 };
 
