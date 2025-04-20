07fb1a69ccac3b46b86708d9e4f02c10cfbc1425
vexuas <rodriguezgab1601@gmail.com>
2025-04-21 01:33:29 +0800

    Export sentry initialisation

--- src/services/instrument.ts
+++ src/services/instrument.ts
@@ -2,12 +2,15 @@ import * as Sentry from '@sentry/node';
 import { SENTRY_DSN_KEY } from '../config/environment';
 import { isEmpty } from 'lodash';
 
-if (SENTRY_DSN_KEY && !isEmpty(SENTRY_DSN_KEY)) {
+export const initializeSentry = () => {
+  if (!SENTRY_DSN_KEY || isEmpty(SENTRY_DSN_KEY)) {
+    console.log('Sentry DSN not found — skipping initialization.');
+    return;
+  }
+
   Sentry.init({
     dsn: SENTRY_DSN_KEY,
   });
 
   console.log('Sentry initialized.');
-} else {
-  console.log('Sentry DSN not found — skipping initialization.');
-}
+};
