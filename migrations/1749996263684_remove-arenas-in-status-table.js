035117b44a180ea70aae88e355ca74c433277a26
vexuas <rodriguezgab1601@gmail.com>
2025-06-15 22:20:03 +0800

    Add schema migration for arena status

--- migrations/1749996263684_remove-arenas-in-status-table.js
+++ migrations/1749996263684_remove-arenas-in-status-table.js
@@ -8,11 +8,37 @@ export const shorthands = undefined;
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const up = (pgm) => {};
+export const up = (pgm) => {
+  pgm.dropColumns('status', [
+    'arenas_channel_id',
+    'arenas_message_id',
+    'arenas_webhook_id',
+    'arenas_webhook_token',
+  ]);
+};
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const down = (pgm) => {};
+export const down = (pgm) => {
+  pgm.addColumns('status', {
+    arenas_channel_id: {
+      type: 'text',
+      notNull: true,
+    },
+    arenas_message_id: {
+      type: 'text',
+      notNull: true,
+    },
+    arenas_webhook_id: {
+      type: 'text',
+      notNull: true,
+    },
+    arenas_webhook_token: {
+      type: 'text',
+      notNull: true,
+    },
+  });
+};
