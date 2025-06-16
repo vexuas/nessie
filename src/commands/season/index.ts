fcdda56d88a2a47ecf559fe6cacfc2889078ae3f
vexuas <rodriguezgab1601@gmail.com>
2025-06-17 01:02:45 +0800

    Add initial embed

--- src/commands/season/index.ts
+++ src/commands/season/index.ts
@@ -1,6 +1,7 @@
-import { SlashCommandBuilder } from 'discord.js';
-import { sendErrorLog } from '../../utils/helpers';
+import { APIEmbed, SlashCommandBuilder } from 'discord.js';
+import { getEmbedColor, sendErrorLog } from '../../utils/helpers';
 import { AppCommand, AppCommandOptions } from '../commands';
+import { getSeasonInformation } from '../../services/adapters';
 
 export default {
   commandType: 'Information',
@@ -10,7 +11,32 @@ export default {
   async execute({ interaction }: AppCommandOptions) {
     try {
       await interaction.deferReply();
-      await interaction.editReply({ embeds: [] });
+      const season = await getSeasonInformation();
+      if (!season) return;
+      const {
+        season: seasonNumber,
+        title,
+        data: { url },
+      } = season.info;
+
+      // const seasonEnd = formatEndDateCountdown({
+      //   endDate: season.dates.end.rankedEnd * 1000,
+      //   currentDate: new Date(),
+      // });
+      // const splitEnd = formatEndDateCountdown({
+      //   endDate: season.dates.split.timestamp * 1000,
+      //   currentDate: new Date(),
+      // });
+
+      const embed: APIEmbed = {
+        title: `Season ${seasonNumber} | ${title}`,
+        color: getEmbedColor(),
+        image: {
+          url,
+        },
+      };
+
+      await interaction.editReply({ embeds: [embed] });
     } catch (error) {
       sendErrorLog({ error, interaction });
     }
