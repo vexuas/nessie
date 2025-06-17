f55eb01e8a1f87669959dccb8df8b591bd9d3878
vexuas <rodriguezgab1601@gmail.com>
2025-06-18 01:41:52 +0800

    Add layout

--- src/commands/season/index.ts
+++ src/commands/season/index.ts
@@ -1,8 +1,14 @@
-import { APIEmbed, SlashCommandBuilder } from 'discord.js';
-import { getEmbedColor, getMapUrl, sendErrorLog } from '../../utils/helpers';
+import { APIEmbed, inlineCode, SlashCommandBuilder } from 'discord.js';
+import {
+  formatEndDateCountdown,
+  getEmbedColor,
+  getMapUrl,
+  sendErrorLog,
+} from '../../utils/helpers';
 import { AppCommand, AppCommandOptions } from '../commands';
 import { getSeasonInformation } from '../../services/adapters';
 import { snakeCase } from 'lodash';
+import { format } from 'date-fns';
 
 export default {
   commandType: 'Information',
@@ -18,30 +24,41 @@ export default {
         season: seasonNumber,
         title,
         description,
+        split,
         data: { image },
       } = season.info;
-      console.log(season);
 
-      // const seasonEnd = formatEndDateCountdown({
-      //   endDate: season.dates.end.rankedEnd * 1000,
-      //   currentDate: new Date(),
-      // });
-      // const splitEnd = formatEndDateCountdown({
-      //   endDate: season.dates.split.timestamp * 1000,
-      //   currentDate: new Date(),
-      // });
+      const seasonEnd = formatEndDateCountdown({
+        endDate: season.dates.end.rankedEnd * 1000,
+        currentDate: new Date(),
+      });
+      const splitEnd = formatEndDateCountdown({
+        endDate: season.dates.split.timestamp * 1000,
+        currentDate: new Date(),
+      });
 
       const embed: APIEmbed = {
         title: `Season ${seasonNumber} | ${title}`,
         color: getEmbedColor(),
-        description,
+        description: `${description}\n\nStarted on: ${inlineCode(
+          format(season.dates.start.timestamp * 1000, 'dd MMM, h:mm a')
+        )}\nCurrent Split: ${inlineCode(split.toString())}`,
         image: {
           url: getMapUrl(`${snakeCase(image)}_rotation`) ?? '',
         },
+        footer: {
+          text: `Season ends on ${format(season.dates.end.rankedEnd * 1000, 'dd MMM, h:mm a')}`,
+        },
         fields: [
           {
-            name: '',
-            value: '',
+            name: 'Split ends in',
+            value: '```fix\n\n' + splitEnd + '```',
+            inline: true,
+          },
+          {
+            name: 'Season ends in',
+            value: '```fix\n\n' + seasonEnd + '```',
+            inline: true,
           },
         ],
       };
